b98c3c54fd2e2589b018cd314bb226375867bcc6
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-04-30 21:19:18 +0300

    dd componentes to home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Button,
+  Container,
+} from "@mui/material";
 
-function App() {
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+import useStyles from "./styles";
+
+const photosCards=[1,2,3,4,5,6,7,8,9,10];
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCameraIcon className={classes.icon} />
+          <Typography variant="h6">This is H6 headline</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom //to add bottom margin
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph //to make sure it loops throw lines
+            >
+              Hello Everyone ! Welcome to my Photo Album ♥ Take a look and hope
+              you will enjoy. You can always be creative in your own way ♥.
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container justifyContent="center" spacing={2}>
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Watch My Photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Help
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container justifyContent='center' spacing={4}>
+            {
+              photosCards.map((index)=>(
+                            <Grid item key={index}>
+              <Card className={classes.cards}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title="Image Title"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5">Head</Typography>
+                  <Typography paragraph>Description of the image</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color='primary'>View</Button>
+                  <Button size="small" color='primary'>Edit</Button>
+
+                </CardActions>
+              </Card>
+            </Grid>
+              ))
+            }
+
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Some information in footer
+        </Typography>
+
+      </footer>
+    </>
   );
-}
+};
 
 export default App;
